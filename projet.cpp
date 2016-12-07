96b7b995e04828e4f0f263976c65d809a293289b
Tsumenokage <jeremy.tsume@gmail.com>
2016-12-07 15:19:48 +0100

    Base structure de projet.cpp

--- projet.cpp
+++ projet.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <sstream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+void process(const char* ims)
+{
+}
+
+
+
+void usage (const char *s)
+{
+	cerr << "Usage:" << s << " ims" << endl;
+	exit(EXIT_FAILURE);	
+}
+
+#define param 1
+
+int main (int argc, char* argv[])
+{
+	if(argc != (param+1))
+		usage(argv[0]);
+	process(argv[1]);
+	return EXIT_SUCCESS;
+		
+}
 
