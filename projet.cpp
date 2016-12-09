eedd0ef793c1963c50857581f18269a508dfe2d6
Tsumenokage <jeremy.tsume@gmail.com>
2016-12-09 11:55:44 +0100

    Plein de truc

--- projet.cpp
+++ projet.cpp
@@ -3,12 +3,28 @@
 #include <time.h>
 #include <sstream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/legacy/legacy.hpp>
 
 using namespace cv;
 using namespace std;
 
 void Hough(Mat);
 void Corner(Mat);
+void CornerFast(Mat);
+void CornerSURF(Mat);
+
+Mat originale;
+Mat imageCorner;
+Mat detectionTerrain;
+int threshCanny = 100;
+RNG rng(12345);
+int cntIndex = 0;
+vector<vector<Point> > contours;
+vector<Point> contourLong;
+vector<Point> coins;
+vector<Point> coinsBut;
 
 void seuillageOtsu(Mat im)
 {
@@ -35,7 +51,7 @@ void detectionCouleur(Mat im)
 	 inRange(imHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgTh); //Threshold the image
 	 
 	 //Hough(im,imgTh);
-	 
+	 detectionTerrain = imgTh;
 	 imshow("couleur",imgTh);
 
 }
@@ -56,6 +72,8 @@ void Hough(Mat im)
 	Canny(grey, outCanny, 0, 255, 3);
 	imshow("Canny",outCanny);
 	Corner(outCanny);
+	CornerFast(outCanny);
+	CornerSURF(outCanny);
 	
 	
 	vector<Vec4i> lines;
@@ -88,6 +106,40 @@ for( size_t i = 0; i < lines.size(); i++ )
 }
 
 
+void CornerFast(Mat im)
+{
+	Mat dst;
+	vector<KeyPoint> detectP;
+	originale.copyTo(dst);
+	FastFeatureDetector fast(150,0);
+	fast.detect(im,detectP);
+	
+	cout << detectP.size() << endl;
+	
+	for (int i = 0; i < detectP.size(); i++)
+	{
+		 circle( dst, detectP[i].pt, 5,  Scalar(0), 2, 8, 0 );
+	}
+	imshow( "coinFAST", dst );
+	
+}
+
+void CornerSURF(Mat im)
+{
+	SURF surf(400);
+	vector<KeyPoint> detectP;
+	Mat dst;
+	originale.copyTo(dst);
+	surf.detect(im,detectP);
+	
+	for (int i = 0; i < detectP.size(); i++)
+	{
+		 circle( dst, detectP[i].pt, 5,  Scalar(0), 2, 8, 0 );
+	}
+	
+	imshow("SURF", dst);
+}
+
 void Corner(Mat im)
 {
   Mat dst;
@@ -97,7 +149,7 @@ void Corner(Mat im)
   int blockSize = 2;
   int apertureSize = 3;
   double k = 0.04;
-  int thresh = 150;
+  int thresh = 100;
 
   /// Detecting corners
   cornerHarris( im, dst, blockSize, apertureSize, k, BORDER_DEFAULT );
@@ -113,27 +165,97 @@ void Corner(Mat im)
             if( (int) dst_norm.at<float>(j,i) > thresh )
               {
                circle( dst_norm_scaled, Point( i, j ), 5,  Scalar(0), 2, 8, 0 );
+               coins.push_back(Point(i,j));
               }
           }
      }
   /// Showing the result
+  imageCorner = dst_norm_scaled;
   imshow( "coin", dst_norm_scaled );
 }
 
+void contoursTerrain()
+{
+	Mat detectionTerrainCanny;
+    vector<Vec4i> hierarchy;
+    int perimeters = 0;
+    Canny( detectionTerrain, detectionTerrainCanny, threshCanny, threshCanny*2, 3 );
+    findContours( detectionTerrainCanny, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  
+  for(int i = 0; i <contours.size();i++)
+  {
+	  int tmpperimeter = arcLength(contours[i],1);
+	  if(tmpperimeter > perimeters)
+	  {
+		  perimeters = tmpperimeter;
+		  cntIndex = i;
+	  }
+	  
+  }
+    
+  /// Draw contours
+  Mat drawing = Mat::zeros( detectionTerrainCanny.size(), CV_8UC3 );
+  Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );     
+  drawContours( drawing, contours, cntIndex, color, 2, 8, hierarchy, 0, Point() );
+  /*
+  for( int i = 0; i< contours.size(); i++ )
+     {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       
+       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+     }*/
+
+  /// Show in a window
+  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+  imshow( "Contours", drawing );
+}
+
+/*
+void Distance()
+{
+	int distance = 999999;
+	int seuilleDistance = 100;
+	Point Pproche;
+	Mat cornerButPic;
+	originale.copyTo(cornerButPic);
+	
+	for (int i = 0; i < coins.size(); i++)
+	{
+		for (int j = 0; j < contours[cntIndex].size(); j++)
+		{
+			if(norm(coins[i],contours[cntIndex][j]) < distance);
+			{
+				Pproche = contours[cntIndex][j];
+				distance = norm(coins[i],contours[cntIndex][j]);
+			}
+		}
+		if(distance < seuilleDistance)
+		{
+				coinsBut.push_back(coins[i]);
+		}
+	}
+	
+
+     for (int i = 0; i < coinsBut.size(); i++)
+	 {
+		 circle( cornerButPic,coinsBut[i], 5,  Scalar(0,0,0), 2, 8, 0 );
+	 }
+	 
+	
+}*/
 
 void process(const char* ims)
 {
 	Mat im = imread(ims);
-	
+	im.copyTo(originale);
 	Hough(im);
-	
-	
+	detectionCouleur(im);
+	contoursTerrain();
 	imshow(ims,im);
 	
 	
 	waitKey(0);
 }
-
 
 
 void usage (const char *s)
--- projet.pro
+++ projet.pro
@@ -16,5 +16,6 @@ LIBS += \
 -lopencv_core \
 -lopencv_highgui \
 -lopencv_imgproc \
+-lopencv_nonfree \
 -lopencv_features2d \
 -lopencv_calib3d
