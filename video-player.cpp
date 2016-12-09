0d9657cae71f11a82df9fec88c0619650145cdbf
ZACCHELLO Jean-baptiste <jzacchel@robinson.pedago.ipb.fr>
2016-12-09 14:11:32 +0100

    projet end

--- video-player.cpp
+++ video-player.cpp
@@ -12,7 +12,90 @@ vector<Mat> images;
 unsigned int image_counter;
 void Hough(Mat);
 void Corner(Mat);
+RNG rng(12345);
 
+int hasValue(Mat image) {
+  int val=0;
+
+  if (image.cols == 0 || image.rows == 0) {
+    return 0;
+  }
+  for(int j=0;j<image.cols;j++) {
+    for(int i=0;i<image.rows;i++) {
+      if( (int) image.at<uchar>(i,j) != 0)
+	return 1;
+    }
+  }
+  return 0;
+}
+
+Mat getPixelMatrix(Mat image, int cols, int rows, int radius) {
+  int minC, maxC, minR, maxR;
+  
+  if ( cols-radius < 0) minC = 0; else minC = cols-radius;
+  if ( cols+radius > image.cols) maxC = image.cols; else maxC = cols+radius;
+
+  if ( rows-radius < 0) minR = 0; else minR = rows-radius;
+  if ( rows+radius > image.rows) maxR = image.rows; else maxR = rows+radius;
+ 
+  Mat submatrix = image.colRange(minC,maxC).rowRange(minR,maxR);    
+  return submatrix;
+}
+
+void patchAll3(const Mat src1, const Mat src2, const Mat src3, Mat& dst) {
+  int taille_patch = 1;
+  src1.copyTo(dst);
+  for(int i=0;i<src1.rows;i++) {
+    for(int j=0;j<src1.cols;j++) {
+      Mat pix1 = getPixelMatrix(src1,j,i,taille_patch);
+      Mat pix2 = getPixelMatrix(src2,j,i,taille_patch);
+      Mat pix3 = getPixelMatrix(src3,j,i,taille_patch);
+      if( hasValue(pix1) && hasValue(pix2) && hasValue(pix3)) {
+	dst.at<uchar>(i,j) = 255;
+      } else {
+	dst.at<uchar>(i,j) = 0;
+      }
+    }
+  }
+}
+
+void contoursTerrain(const Mat src, Mat &dst)
+{
+  int threshCanny = 100;
+  vector<vector<Point> > contours;
+  Mat detectionTerrainCanny;
+  vector<Vec4i> hierarchy;
+  int perimeters = 0;
+  int cntIndex = 0;
+  Canny( src, detectionTerrainCanny, threshCanny, threshCanny*2, 3 );
+  findContours( detectionTerrainCanny, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  
+  for(int i = 0; i <contours.size();i++)
+    {
+      int tmpperimeter = arcLength(contours[i],1);
+      if(tmpperimeter > perimeters)
+	{
+	  perimeters = tmpperimeter;
+	  cntIndex = i;
+	}
+      
+    }
+  
+  /// Draw contours
+  Mat drawing = Mat::zeros( detectionTerrainCanny.size(), CV_8UC3 );
+  Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );     
+  drawContours( drawing, contours, cntIndex, color, 2, 8, hierarchy, 0, Point() );
+
+  cvtColor(drawing,drawing,CV_BGR2GRAY);
+  for(int i=0;i<drawing.rows;i++) {
+    for(int j=0;j<drawing.cols;j++) {
+      int pixel = drawing.at<uchar>(i,j);
+      if(pixel > 0)
+	drawing.at<uchar>(i,j) = 255;
+    }
+  }
+  drawing.copyTo(dst);
+}
 void detectionCouleur(const Mat im, Mat &dst)
 {
 	 Mat imgTh;
@@ -216,14 +299,20 @@ void process() {
       erode(grass,grass,cv::Mat());
       dilate(grass,grass,cv::Mat());
       
-      Mat black = Mat::zeros(image.rows,image.cols,CV_8UC1);
+      Mat countour;
+      contoursTerrain(grass,countour);
       Mat input[3];
       input[0] = HS;
       input[1] = grass;
-      input[2] = black;
+      input[2] = countour;
       Mat output;
       merge(input,3,output);
       imshow("output",output);
+
+      Mat last;
+      patchAll3(grass, HS, countour, last) ;
+      imshow("last",last);
+
       
       /************* HoughLines *******************/
       
